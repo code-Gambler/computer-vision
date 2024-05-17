5043666cfd5f50b3ef68c5e2ba4ca94867936897
code-Gambler <spillay6@myseneca.ca>
2024-05-17 18:45:30 -0400

    Added Workshop 2

---
+++ Workshops/workshop-2/code/question1.py
@@ -0,0 +1,54 @@
+
+"""
+Group Number: ..........
+Group Names :
+     Last name , First name
+     .......
+     .......
+
+
+"""
+
+
+
+"""
+Question 1: Getting familiar with image manipulation in python - opencv
+Read the image "bicycle.bmp" from the <data> directory
+using opencv flag cv2.IMREAD_UNCHANGED
+and then find and print the following information about this image
+
+a)	find image height (number of rows)
+b)	find image width (number of columns)
+c)	find image number of channels
+d)	find image datatype
+e)	find image number of pixels
+f)	convert the image to gray level and then save it in output directory as "bicyclegray.jpg"
+g)	find the maximum value of the pixel values
+h)	Calculate the mean/average of the pixel values
+i)	Change the pixel values of the image in the following way:
+    all pixels’ values less than the average value calculated at (h) will be equal to 0
+    and all the other pixels will be equal to 1. Then, save it in output directory as "bicycleoutA.jpg"
+j)	What type of image that is  generated at (i)?
+
+
+
+
+no need to do this
+Repeat the about information using the following opencv flag
+cv2.IMREAD_COLOR, cv2.IMREAD_GRAYSCALE
+
+"""
+import cv2
+import numpy as np
+
+if __name__ == '__main__':
+
+
+    ############################
+    ### TODO: YOUR CODE HERE ###
+
+    raise NotImplementedError('Implementation for `Question ` in `workshop2` is missing')
+
+    ### END OF STUDENT CODE ####
+    ############################
+
---
+++ Workshops/workshop-2/code/question2.py
@@ -0,0 +1,38 @@
+"""
+Group Number: ..........
+Group Names :
+     Last name , First name
+     .......
+     .......
+"""
+
+
+"""
+Question 2:
+Reducing the Number of Intensity Levels in an Image
+Write a computer program capable of reducing the number of intensity levels in a gray image from 256 levels to 2 levels,
+in integer powers of 2. That is intensity levels: 256 default, 128, 64, 32, 16, 8, 4, 2.
+Use image "lena.tif" from data directory
+Note: Your code should generate the image with the required intensity and then write/save the image
+to the output directory.
+The name of generated images as following lena256.jpg, lena128.jpg, lena64.jpg, lena32.jpg, lena16.png,
+ lena8.png, lena4.png, and lena2.png.
+
+
+
+
+"""
+import cv2
+import numpy as np
+
+if __name__ == '__main__':
+
+
+    ############################
+    ### TODO: YOUR CODE HERE ###
+
+    raise NotImplementedError('Implementation for `Question ` in `workshop2` is missing')
+
+    ### END OF STUDENT CODE ####
+    ############################
+
